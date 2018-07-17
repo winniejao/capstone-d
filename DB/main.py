1021a79f622ea149e9defbbd189d4c10472457e6
Ben <bbruser885@gmail.com>
2018-07-16 20:25:40 -0700

    main.py with 7/16 stuff

--- DB/main.py
+++ DB/main.py
@@ -4,8 +4,44 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def getRequest():
+################ FORM BANK ##################
+@app.route('/equipment/form', methods=['POST'])
+def rAddForm():
+    try:
+        getRequest = json.loads(request.data)
+        print getRequest
+        return "form POST Success!"
+    except:
+        return "Error: form POST failed"
+
+@app.route('/equipment/form<id>', methods=['GET'])
+def rGetForm():
+        try:
+            result = "form GET Success!"
+            return jsonify(result)
+        except:
+             return "Error: form GET failed"
+
+@app.route('/equipment/form<id>', methods=['DELETE'])
+def rDeleteForm():
+    try:
+        getRequest = json.loads(request.data)
+
+        return "form DELETE Success!"
+    except:
+        return "Error: form DELETE failed"
+
+@app.route('/equipment/form<id>', methods=['PUT'])
+def rAlterForm():
+    try:
+        getRequest = json.loads(request.data)
+        return "fomr PUT Success!"
+    except:
+        return "Error: form PUT failed"
+
+###### NEW SUBCATEGORY BANK ############
+@app.route('/landscape/sub/<name>', methods=['POST'])
+def rLandscapeSub():
     try:
         getRequest = json.loads(request.data)
         print getRequest
@@ -13,10 +49,45 @@ def getRequest():
     except:
         return "Error: POST failed"
 
-@app.route('/', methods=['GET'])
-def sendResponse():
+@app.route('/equipment/sub/<name>', methods=['POST'])
+def rEquipmentSub():
     try:
-        result = "Success"
+        getRequest = json.loads(request.data)
+        print getRequest
+        return "POST Success!"
+    except:
+        return "Error: POST failed"
+
+@app.route('/tools/sub/<name>', methods=['POST'])
+def rToolSub():
+    try:
+        getRequest = json.loads(request.data)
+        print getRequest
+        return "POST Success!"
+    except:
+        return "Error: POST failed"
+
+###### GET BANK ##############
+@app.route('/landscape/<string>', methods=['GET'])
+def rLandscapeGet():
+    try:
+        result = "landscape GET Success"
+        return jsonify(result)
+    except:
+         return "Error: GET failed"
+
+@app.route('/equipment/<string>', methods=['GET'])
+def rEquipmentGet():
+    try:
+        result = "equipment GET Success"
+        return jsonify(result)
+    except:
+         return "Error: GET failed"
+
+@app.route('/tools/<string>', methods=['GET'])
+def rToolsGet():
+    try:
+        result = "tools GET Success"
         return jsonify(result)
     except:
          return "Error: GET failed"
