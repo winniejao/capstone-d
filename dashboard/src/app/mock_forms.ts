fb7a6822732b8450e211223e7cc3636fb280a1ec
David Stamper <stamper.dt@gmail.com>
2018-08-08 23:09:02 -0700

    Fixed compilation errors after merge

--- dashboard/src/app/mock_forms.ts
+++ dashboard/src/app/mock_forms.ts
@@ -4,11 +4,11 @@
 import { Form } from './form'
 
 export const MOCK_FORMS: Form[] = [
-  { formid: 1, subcat: 'Boiler', name: 'Boiler 1', item: 'Wood Pellet', purpose: 'To Burn', cost: 24.99, serial: '134-567-890', date: '07-03-2018', attach: null, notes: 'All of this should be burned at once' },
-  { formid: 2, subcat: 'Boiler', name: 'Boiler 2', item: 'Wood Pellet', purpose: 'To Burn', cost: 24.99, serial: '134-567-890', date: '07-03-2018', attach: null, notes: 'All of this should be burned at once' },
-  { formid: 3, subcat: 'Boiler', name: 'Boiler 3', item: 'Wood Pellet', purpose: 'To Burn', cost: 24.99, serial: '134-567-890', date: '07-03-2018', attach: null, notes: 'All of this should be burned at once' },
-  { formid: 4, subcat: 'Boiler', name: 'Boiler 4', item: 'Wood Pellet', purpose: 'To Burn', cost: 24.99, serial: '134-567-890', date: '07-03-2018', attach: null, notes: 'All of this should be burned at once' },
-  { formid: 5, subcat: 'Stove', name: 'Stove 1', item: 'Gas', purpose: 'To Burn', cost: 10.99, serial: '034-567-840', date: '07-03-2018', attach: null, notes: 'Poof' }
+  { formid: 1, category: "Equipment", subcat: 'Boiler', name: 'Boiler 1', item: 'Wood Pellet', purpose: 'To Burn', cost: 24.99, serial: '134-567-890', date: '07-03-2018', attachment: null, notes: 'All of this should be burned at once', maint_date: '2018-09-12', repeat: 0 },
+  { formid: 2, category: "Equipment", subcat: 'Boiler', name: 'Boiler 2', item: 'Wood Pellet', purpose: 'To Burn', cost: 24.99, serial: '134-567-890', date: '07-03-2018', attachment: null, notes: 'All of this should be burned at once', maint_date: '2018-09-12', repeat: 0 },
+  { formid: 3, category: "Equipment", subcat: 'Boiler', name: 'Boiler 3', item: 'Wood Pellet', purpose: 'To Burn', cost: 24.99, serial: '134-567-890', date: '07-03-2018', attachment: null, notes: 'All of this should be burned at once', maint_date: '2018-09-12', repeat: 0 },
+  { formid: 4, category: "Equipment", subcat: 'Boiler', name: 'Boiler 4', item: 'Wood Pellet', purpose: 'To Burn', cost: 24.99, serial: '134-567-890', date: '07-03-2018', attachment: null, notes: 'All of this should be burned at once', maint_date: '2018-09-12', repeat: 0 },
+  { formid: 5, category: "Equipment", subcat: 'Stove', name: 'Stove 1', item: 'Gas', purpose: 'To Burn', cost: 10.99, serial: '034-567-840', date: '07-03-2018', attachment: null, notes: 'Poof', maint_date: '2018-09-12', repeat: 0 }
 ];
 
 export const MOCK_NUMBER: number = 5;
