99364eac1111cd780b1f45145bee65cbd6703a0c
David Stamper <stamper.dt@gmail.com>
2018-08-12 14:48:56 -0700

    Added a deeper module to test the routing, better route construction

--- dashboard/src/app/breadcrumb/breadcrumb.component.ts
+++ dashboard/src/app/breadcrumb/breadcrumb.component.ts
@@ -17,26 +17,19 @@ export class BreadcrumbComponent implements OnInit {
   routerEvent: any; //if I need to unsubscribe
 
   parseTrail(input: string): void {
-    this.crumbtrail = input.trim().split('/').filter( x => x != '').filter(x => x != 'mainpage');
+    this.crumbtrail = input.trim()
+      .split('/')
+      .filter( x => x != '')
+      .filter(x => x != 'mainpage');
     this.crumtrailActive = this.crumbtrail.pop();
-    // https://stackoverflow.com/questions/38168408/how-to-implement-multi-level-routing-in-angular
   }
 
-  reconstruct(input: string): string {
-    var slashes = this.crumbtrail.map( x => x + '/');
-    var reconstructing = [];
-    slashes.forEach( x => {
-      if(x != input){
-        reconstructing.push(x);
-      } else {
-        reconstructing.push(x);
-        return;
-      }
-    });
-    var reconstructed = reconstructing.join(); 
-    console.log(reconstructed, input);
-    return reconstructed;
-  }
+  
+  reconstruct(input): string {
+    const indexOf = this.crumbtrail.findIndex(v => v === input);
+    if (indexOf === -1) { return ''; }
+    return this.crumbtrail.slice(0, indexOf+1).join('/');    
+}
 
   ngOnInit() {
 
@@ -46,14 +39,6 @@ export class BreadcrumbComponent implements OnInit {
       .subscribe( event => {
         this.parseTrail(event.urlAfterRedirects);
       });
-
-    /* Alternative way of setting it up without rxjs pipes
-    this.routerEvent = this.router.events.subscribe( (event:NavigationEnd) => {
-      if(event instanceof NavigationEnd){
-        console.log(event.urlAfterRedirects);        
-      }
-    })
-    */
   }
   
 
