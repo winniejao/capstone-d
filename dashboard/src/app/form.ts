b5212ff667b41fe78e7acbd53ecf6555be0281e2
David Stamper <stamper.dt@gmail.com>
2018-08-20 02:22:28 -0700

    Missed one working change

--- dashboard/src/app/form.ts
+++ dashboard/src/app/form.ts
@@ -18,21 +18,7 @@ export class Form {
           this.attachment = "C:/testpath";
           this.notes = "TestNote";
     }
-    constructor(id: number, cat: string, scat: string, name: string){
-        this.formid = id;
-        this.category = cat;
-        this.subcat = scat;
-        this.name = "TestName";
-        this.item = "TestItem";
-        this.purpose = "TestPurpose";
-        this.cost = 3.14;
-        this.serial = "ATES-TSER-IAL1";
-        this.date = "11/22/2018";
-        this.maint_date = "2018-10-10";
-        this.repeat = 2;
-        this.attachment = "C:/testpath";
-        this.notes = "TestNote";
-  }
+    
     formid: number;
     category: string;
     subcat: string;
