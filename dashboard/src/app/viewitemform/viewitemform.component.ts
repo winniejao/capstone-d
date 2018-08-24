2547739b8dd2710ffb81243a9defefe19be469a6
Wenli Jao <leeeun@pdx.edu>
2018-08-23 18:32:30 -0700

    changes in view form

--- dashboard/src/app/viewitemform/viewitemform.component.ts
+++ dashboard/src/app/viewitemform/viewitemform.component.ts
@@ -28,4 +28,9 @@ export class ViewitemformComponent implements OnInit {
     this.dService.addForm(this.table_details);
   }
 
+  deleteData(id, cat, sub, name, item, purpose, cost, serial, date, from,  every, dwm, note, attach) {
+    this.iService.setData(id, cat, sub,name, item, purpose,cost,serial,date, from,every,dwm,note,attach);
+    this.table_details={form_id:id, category:cat, subcat:sub, name:name, item:item, purpose:purpose, cost:cost, serial:serial, date:date, maint_date:from, repeat:every, attach:attach, notes:note};
+    this.dService.deleteForm(this.table_details);
+  }
 }
