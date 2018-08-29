40f7ee0f8d293164f419ab382c208170fcdb507b
David Stamper <stamper.dt@gmail.com>
2018-08-29 15:23:42 -0700

    Got electron file browsing finished removing id

--- dashboard/src/app/additemform/additemform.component.ts
+++ dashboard/src/app/additemform/additemform.component.ts
@@ -4,6 +4,8 @@ import { ItemfieldsService } from '../itemfields.service';
 import { PassServiceService } from '../pass-service.service';
 import { DashService } from '../dash.service';
 import { Form } from '../form';
+import { ElectronService } from 'ngx-electron';
+
 
 @Component({
   selector: 'app-additemform',
@@ -12,24 +14,42 @@ import { Form } from '../form';
 })
 export class AdditemformComponent implements OnInit {
 
-  table_details: Form
+  table_details: Form;
+
   constructor(
 	private location: Location, 
 	private iService: ItemfieldsService,
-	public pService: PassServiceService,
+  public pService: PassServiceService,
+  private _electronService: ElectronService,
 	private dService: DashService
   ) { }
 
+  selectedFiles: string[];
+
   ngOnInit() {
+  }
+
+  addAttachments(): void {
+    const path = this._electronService.remote.dialog.showOpenDialog( { properties: ['multiSelections', 'openFile']});
+    console.log(path);
+    if(path && path.length) {
+      this.selectedFiles = path;
+    }
+
+  }
+
+  remove(index: number) {
+    this.selectedFiles.splice(index, 1);
   }
 
   alertCancel() {
     this.location.back();
   }
 
-  passData(id, cat, sub, name, item, purpose, cost, serial, date, from,  every, dwm, note, attach) {
-    this.iService.setData(id, cat, sub,name, item, purpose,cost,serial,date, from,every,dwm,note,attach);
-    this.table_details={form_id:id, category:cat, subcat:sub, name:name, item:item, purpose:purpose, cost:cost, serial:serial, date:date, maint_date:from, repeat:every, attach:attach, notes:note};
+  //attach removed and is now a property of the component
+  passData(id, cat, sub, name, item, purpose, cost, serial, date, from,  every, dwm, note) {
+    this.iService.setData(id, cat, sub,name, item, purpose,cost,serial,date, from,every,dwm,note,this.selectedFiles);
+    this.table_details={form_id:id, category:cat, subcat:sub, name:name, item:item, purpose:purpose, cost:cost, serial:serial, date:date, maint_date:from, repeat:every, attach:this.selectedFiles, notes:note};
 
     this.dService.addForm(this.table_details).subscribe();
   }
