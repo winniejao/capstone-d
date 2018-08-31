f903c959dc7f1725ee36d1c76cbea6aa2d9b4848
ryant <ryanlui.college@gmail.com>
2018-08-31 11:17:40 -0700

    Search result now responds to repeated searches

--- dashboard/src/app/search-result/search-result.component.ts
+++ dashboard/src/app/search-result/search-result.component.ts
@@ -1,36 +1,49 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, ViewChild } from '@angular/core';
 import { SearchResult } from '../search-result';
 import { ArrayResponse } from '../master-service';
 import { DashService } from '../dash.service';
-// TODO: use real service
-import { Form, FORM_HEADERS } from '../form';
+import { Form, FORM_HEADERS_XREPEAT } from '../form';
 import { MatTableModule, MatTableDataSource, MatSort, MatPaginator } from '@angular/material/';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { MOCK_FORMS } from '../mock_forms';
+
+// For refresh on repeated search: https://medium.com/engineering-on-the-incline/reloading-current-route-on-click-angular-5-1a1bfc740ab2
 
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
   styleUrls: ['./search-result.component.css']
 })
-export class SearchResultComponent implements OnInit {
+export class SearchResultComponent implements OnInit, OnDestroy {
   search_term: string;
   table_details: Form[];
-  table_columns: string[] = FORM_HEADERS;
+  table_columns: string[] = FORM_HEADERS_XREPEAT;
   dataSource: MatTableDataSource<Form>;
   pageSize = 10;
   pageSizeOptions: number[] = [10, 20, 30, 40, 50];
   num_results: number;
+  navigationSubscription;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private service: DashService,
-    private route: ActivatedRoute
-  ) { }
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {
+    // subscribe to the router events - storing the subscription so
+    // we can unsubscribe later.
+    this.navigationSubscription = this.router.events.subscribe((e: any) => {
+      // If it is a NavigationEnd event re-initalise the component
+      if (e instanceof NavigationEnd) {
+        this.initialiseInvites();
+      }
+    });
+  }
 
-  ngOnInit() {
+  initialiseInvites() {
+    // Set default values and re-fetch any data you need.
     this.search_term = this.route.snapshot.paramMap.get('search_term');
     this.service.search(this.search_term).subscribe(table_details => {
       this.table_details = table_details;
@@ -40,5 +53,18 @@ export class SearchResultComponent implements OnInit {
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
     });
+  }
+
+  ngOnInit() {
+    
+  }
+
+  ngOnDestroy() {
+    // avoid memory leaks here by cleaning up after ourselves. If we  
+    // don't then we will continue to run our initialiseInvites()   
+    // method on every navigationEnd event.
+    if (this.navigationSubscription) {
+      this.navigationSubscription.unsubscribe();
+    }
   }
 }
